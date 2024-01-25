3df4ab59c4cc7668226e9139e76fed773bcea7fe
Conan <conanchou1412@gmail.com>
2024-01-25 17:02:19 -0500

    Initial Commit

---
+++ demo project/demo_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_projectApp.swift
+//  demo project
+//
+//  Created by Conan Chou on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct demo_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
