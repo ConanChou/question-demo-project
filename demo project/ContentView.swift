87613fe4850ad4627d1e6409ccd7dce8ed2a0bb7
Conan <conanchou1412@gmail.com>
2024-01-25 17:50:39 -0500

    init

--- demo project/ContentView.swift
+++ demo project/ContentView.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import AppKit
+import QuickLookUI
+
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                if let path = Bundle.main.path(forResource: "quote", ofType: "png") {
+                    let url = URL(fileURLWithPath: path)
+                    print(url)
+                    let panelController = QuotesQLViewController()
+                    panelController.previewFile(self, url: url)
+                } else {
+                    print("not found")
+                }
+            } label: {
+                Text("click me")
+            }
+
         }
         .padding()
     }
@@ -21,4 +33,48 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+class QuotesQLViewController: NSViewController, QLPreviewPanelDataSource, QLPreviewPanelDelegate {
+    var url: URL!
+    
+    @objc func previewFile(_ sender: Any, url: URL) {
+        self.url = url
+        if let panel = QLPreviewPanel.shared() {
+            panel.dataSource = self
+            panel.delegate = self
+            panel.makeKeyAndOrderFront(nil)
+        }
+        
+    }
+    
+    @objc func copyToClipboard() {
+        // read the image data from the url into memory
+        guard let image = NSImage(contentsOf: self.url) else { return }
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.writeObjects([image])
+    }
+    
+    override func acceptsPreviewPanelControl(_ panel: QLPreviewPanel!) -> Bool {
+        return true
+    }
+
+    override func beginPreviewPanelControl(_ panel: QLPreviewPanel!) {
+        panel.dataSource = self
+        panel.delegate = self
+    }
+
+    override func endPreviewPanelControl(_ panel: QLPreviewPanel!) {
+        panel.dataSource = nil
+        panel.delegate = nil
+    }
+
+    func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int {
+        return 1
+    }
+
+    func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! {
+        return self.url as QLPreviewItem
+    }
 }
